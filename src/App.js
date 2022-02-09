d4b92d5a06cbd00d896a948f7296814b7f955832
ckatjrdavis <ckatjrdavis@gmail.com>
2022-02-09 15:04:30 -0600

    Created DisplayPosts component and instantiated component on app component

--- src/App.js
+++ src/App.js
@@ -1,23 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import DisplayPosts from './Components/DisplayPosts/DisplayPosts';
+
 
 function App() {
+
+  const [posts, setPosts] = useState([{name: 'Chris', post: 'This is a mock post'}])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <DisplayPosts parentPosts={posts} />
+      
     </div>
   );
 }
